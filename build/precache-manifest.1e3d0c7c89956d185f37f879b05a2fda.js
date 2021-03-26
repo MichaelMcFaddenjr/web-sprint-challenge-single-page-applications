142bd88f2c7524c6d7383a138a74da55db81995e
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-03-26 10:47:57 -0700

    Completed MVP, need to work on styling

---
+++ build/precache-manifest.1e3d0c7c89956d185f37f879b05a2fda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f57febdbd4136299f96cf5c9d593a84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd4deea3d730e41a4e3b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "80c891ce177ded7b4e56",
+    "url": "/static/js/2.a138c49c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a138c49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4deea3d730e41a4e3b",
+    "url": "/static/js/main.60089618.chunk.js"
+  },
+  {
+    "revision": "545a27a708665afc91a8",
+    "url": "/static/js/runtime-main.e36952ae.js"
+  }
+]);
---
+++ src/validation/formSchema.js
@@ -0,0 +1,22 @@
+import * as Yup from "yup";
+
+export default Yup.object().shape({
+    name: Yup
+    .string()
+    .required("Name is required")
+    .min(5, "Name must be at least 5 characters long"),
+    email: Yup
+    .string()
+    .email("must be a valid email")
+    .required("email is required"),
+    size: Yup
+    .string()
+    .required("Size is required"),
+    peppers: Yup.boolean(),
+    onions: Yup.boolean(),
+    olives: Yup.boolean(),
+    pepperoni: Yup.boolean(),
+    anchovies: Yup.boolean(),
+    pineapple: Yup.boolean(),
+    specialInstructions: Yup.string()
+})
